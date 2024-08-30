d7136e425355dc71f7125543109414a7a107c39b
pvcordeiro <pv.35@hotmail.com>
2024-08-30 22:28:25 +0100

    finished game

--- script.js
+++ script.js
@@ -1,44 +1,68 @@
 let humanScore = 0;
 let computerScore = 0;
 
+alert("Feeling lucky? The first to score 5 points wins!!!");
 const getComputerChoice = () => {
     const options = ["rock", "paper", "scissors"];
     const random = Math.floor(Math.random() * 3);
     const choice = options[random];
-    console.log(choice);
     return choice;
 };
-
-// getComputerChoice();
 
 const getHumanChoice = () => {
     let input = prompt("Please type Rock, Paper or Scissors");
 
     const choice = input.toLowerCase();
 
-    console.log(choice);
-    return choice;
+    if (choice === "") {
+        alert("Please type something!!!");
+        return getHumanChoice();
+    } else if (
+        choice !== "rock" &&
+        choice !== "paper" &&
+        choice !== "scissors"
+    ) {
+        alert("Please type only one of the three options");
+        return getHumanChoice();
+    } else {
+        return choice;
+    }
 };
 
-// getHumanChoice();
+const playRound = () => {
+    human = getHumanChoice();
+    computer = getComputerChoice();
 
-const playRound = (humanChoice, computerChoice) => {
-    humanChoice = getHumanChoice();
-    computerChoice = getComputerChoice();
-
-    if (humanChoice === computerChoice) {
-        return alert("It's a tie, GO AGANE!");
+    if (human === computer) {
+        alert("It's a tie, GO AGANE!");
     }
 
     if (
-        (humanChoice === "rock" && computerChoice === "scissors") ||
-        (humanChoice === "scissors" && computerChoice === "paper") ||
-        (humanChoice === "paper" && computerChoice === "rock")
+        (human === "rock" && computer === "scissors") ||
+        (human === "scissors" && computer === "paper") ||
+        (human === "paper" && computer === "rock")
     ) {
-        return alert(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+        alert(`You win the round! ${human} beats ${computer}`);
     } else {
-        return alert();
+        computerScore++;
+        alert(`You lose the round! ${computer} beats ${human}`);
     }
 };
 
-const playGame = () => {};
+const playGame = () => {
+    humanScore = 0;
+    computerScore = 0;
+
+    while (humanScore < 5 && computerScore < 5) {
+        playRound();
+        alert(`Your Score: ${humanScore}. Computer score: ${computerScore}.`);
+    }
+    if (humanScore === 5) {
+        alert("Nice! You're lucky");
+    } else {
+        alert("You lose! RIP BOZO");
+    }
+};
+
+playGame();
